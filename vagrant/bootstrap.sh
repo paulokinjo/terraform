c2f58a5c212df35038ddc3d3ac44e3973ab7072e
Paulo Kinjo <paulokinjo@hotmail.com>
2022-01-20 13:18:34 +0900

    (vagrant): update image

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,2 +1,11 @@
-echo "[Task - 1] Install terraform"
-sudo snap install --candidate terraform
+echo "[Task - 1] Add the keys"
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+
+echo "[Task - 2] Add the repositories"
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
+echo "[Task - 3] Update the OS"
+sudo apt-get update
+
+echo "[Task - 4] Install Terraform"
+sudo apt-get install terraform
